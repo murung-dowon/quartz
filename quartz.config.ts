550c60318bf08b3efe9911e141e290f3bce231ab
murung-dowon <murung@tonys-MacBook-Pro-2.local>
2024-03-15 14:54:03 +0900

    Quartz sync: Mar 15, 2024, 2:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: 'goatcounter', websiteId: 'murung'
     },
     locale: "ko-KR",
-    baseUrl: "https://quartz-5ll.pages.dev/",
+    baseUrl: "murung.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#2c5225",
+          secondary: "#253b2b",
           tertiary: "#2c5225",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
@@ -43,8 +43,8 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
+          secondary: "#253b2b",
+          tertiary: "#2c5225",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
       },
