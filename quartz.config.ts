0744e64be5f951ca5a881845978603bfb2179224
murung-dowon <murung@tonys-MacBook-Pro-2.local>
2024-03-15 19:42:27 +0900

    Quartz sync: Mar 15, 2024, 7:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           light: "#ffffff", // page background
-          lightgray: "#adadad", // borders
+          lightgray: "#e0e0e0", // borders
           gray: "#2e2e2e", // graph links, heavier borders
           darkgray: "#4e4e4e", // body text
           dark: "#2b2b2b", // header text and icons
@@ -34,7 +34,7 @@ const config: QuartzConfig = {
         },
         darkMode: {
           light: "#1c2127",
-          lightgray: "f0f0f0",
+          lightgray: "#f0f0f0",
           gray: "#f0f0f0",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
